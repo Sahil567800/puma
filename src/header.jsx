35e356d9a57a33b42930e8f40c46bf4205bd0e84
Sahil567800 <sahilskp110@gmail.com>
2024-11-08 00:07:12 +0530

    nav bar complete

--- src/header.jsx
+++ src/header.jsx
@@ -4,9 +4,18 @@ import logo from "./assets/pumalogo3.png";
 import { MdAccountCircle } from "react-icons/md";
 import { FaShoppingCart } from "react-icons/fa";
 import { useState } from "react";
+import { FaAngleUp } from "react-icons/fa";
 const Header = () => {
     const [searchToggle, setSearch] = useState(false)
     const [inputChange, setInputChange] = useState("")
+    const [isnav,setNav] = useState(false)
+    let bars = <FaBars/>;
+    if(isnav===true){
+        bars = <FaAngleUp/>
+    }
+    const handleNav =()=>{
+        setNav(!isnav)
+    }
     const handleChange = (e) => {
         setInputChange(e.target.value)
     }
@@ -14,25 +23,40 @@ const Header = () => {
         setSearch(true)
     }
     let search;
+    let mSearch;
     if (!searchToggle) {
-        search = <div className="search"><button className="flex" onClick={() => toggleSearch()}><FaMagnifyingGlass />SEARCH</button></div>
+        search = <div className="search"><button className="flex" onClick={() => toggleSearch()}>SEARCH</button></div>
+        mSearch =<FaMagnifyingGlass/>
     }
     else {
         search = <div className="search-bar">
             <input type="text" placeholder="ENTER PRODUCT NAME" value={inputChange} onChange={handleChange} />
             <button><FaMagnifyingGlass /></button>
         </div>
+        mSearch = 
+        <input type="text" placeholder="ENTER PRODUCT NAME" value={inputChange} onChange={handleChange} />
     }
+  
     return (
         <>
             <header className="w-full bg-black text-white items-center">
                 <div className="continer">
                     <div className="raw">
-                        <div className="bars"><FaBars /></div>
+                        <div className="bars"  onClick={handleNav}>{bars}</div>
                         {search}
-                        <div className="m-search"><FaMagnifyingGlass /></div>
+                        <div className="m-search" onClick={()=>handleChange()}>{mSearch}</div>
                         <div className="logo"> <img src={logo} alt="" width={"50px"} /></div>
                         <div className="cart"><MdAccountCircle /><FaShoppingCart /></div>
+                    </div>
+                    <div className="raw">
+                        <nav className={`${!isnav?"None":"flex"}`}>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Services</li>
+                        <li>Products</li>
+                        <li>Contact uS</li>
+                        </nav>
+                        
                     </div>
                 </div>
 
